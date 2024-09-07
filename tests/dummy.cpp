79de478a4cbba6b790e317a03c21291b49aafeca
Neeraj Jakhar <neeraj.jakhar@gmail.com>
2024-09-08 01:32:44 +0530

    Fix build issue

--- tests/dummy.cpp
+++ tests/dummy.cpp
@@ -1,11 +1,17 @@
+/*
+ * Copyright (c) 2024 Neeraj Jakhar
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include "doctest/doctest.h"
-#include "example.hpp"
 
 // Tests that don't naturally fit in the headers/.cpp files directly
 // can be placed in a tests/*.cpp file. Integration tests are a good example.
 
 TEST_CASE("complicated integration tests could be here")
 {
-  Dummy d;
-  CHECK(d.doSomething() == true);
+  CHECK(true == true);
 }
