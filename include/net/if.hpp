e6b1392466d9810b3f7684debe3dd5b88971a737
Neeraj Jakhar <31380679+slashblog@users.noreply.github.com>
2024-09-27 02:32:16 +0530

    Some minor optimizations (#8)
    * Draft rules implementation
    
    * Fix iterator
    
    * Windows build fixes
    
    * Formatting changes
    
    * Header fixes
    
    * Windows build fixes
    
    * Draft implementation of Ethernet address processing
    
    * Fix windows build issue
    
    * Shm rule evaluation draft changes
    
    * Implement fixes
    
    * Adding missing file
    
    * Windows build fixes
    
    * Formatting changes
    
    * Windows build fixes
    
    * Rule implementation
    
    * Windows build fixes
    
    * Formatting changes
    
    * Getting rid of endpoint
    
    * Fix for reverse order read/write for Class and Type
    
    * Windows release build fixes
    
    * Some code optimizations
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Neeraj Jakhar <neeraj.jakhar@gmail.com>

--- include/net/if.hpp
+++ include/net/if.hpp
@@ -6,15 +6,24 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#pragma once
+
 #include <cstddef>
 #include <string>
 
-template <class T> class Interface {
+#ifdef __linux
+#include <ifaddrs.h>
+#define IF_CLASS ifaddrs
+#else /* WIN32 */
+#define IF_CLASS IP_ADAPTER_ADDRESSES
+#endif /* __linux */
+
+class Interface {
 private:
-  T *ptr;
+  IF_CLASS *ptr;
 
 public:
-  Interface(T *addrs) : ptr(addrs) {}
+  Interface(IF_CLASS *addrs) : ptr(addrs) {}
   ~Interface() {}
 
   int Index() const;
@@ -22,39 +31,33 @@ public:
   std::string Type() const;
   bool HasIpv4() const;
   bool HasIpv6() const;
-  T *Get() { return ptr; }
-  Interface<T> &operator++() {
-    ++ptr;
-    // prefix - this will be implemented only in specialization
-    return *this;
-  }
-  bool operator==(const Interface<T> &b) { return ptr == b.ptr; }
-  bool operator!=(const Interface<T> &b) { return ptr != b.ptr; }
+  IF_CLASS *Get() { return ptr; }
+  Interface &operator++();
+  bool operator==(const Interface &b) { return ptr == b.ptr; }
+  bool operator!=(const Interface &b) { return ptr != b.ptr; }
 };
 
-template <class T> class NetInterface {
+class NetInterface {
 public:
   class Iterator {
   public:
     using difference_type = std::ptrdiff_t;
-    using element_type = Interface<T>;
+    using element_type = Interface;
     using pointer = element_type *;
     using reference = element_type &;
 
   private:
-    Interface<T> *_interface;
+    Interface *_interface;
 
   public:
-    Iterator(T *p) : _interface(new Interface<T>(p)) {}
+    Iterator(IF_CLASS *p) : _interface(new Interface(p)) {}
     ~Iterator() {
       if (_interface) {
         delete _interface;
       }
     }
-    Iterator(Iterator &b) {
-      _interface = new Interface<T>(b._interface->Get());
-    }
-    Iterator(Iterator &&b) {
+    Iterator(Iterator &b) { _interface = new Interface(b._interface->Get()); }
+    Iterator(Iterator &&b) noexcept {
       _interface = b._interface;
       b._interface = nullptr;
     }
@@ -73,13 +76,13 @@ public:
     bool operator!=(const Iterator &b) { return *_interface != *b._interface; }
   };
 
-  NetInterface() : addrs(nullptr), start(nullptr), finish(nullptr) {}
-  ~NetInterface() {}
+  NetInterface();
+  ~NetInterface();
   Iterator begin() { return start; }
   Iterator end() { return finish; }
 
 private:
-  T *addrs;
+  IF_CLASS *addrs;
   Iterator start;
   Iterator finish;
 };
